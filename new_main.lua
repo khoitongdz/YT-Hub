6452bf15dfc6d7c8b4a6245b382a0da267ed1f8a
Khoitongdz <khoitong0000@gmail.com>
2025-03-17 13:51:10 +0700

    Update and rename new_main.lua to main.lua

--- new_main.lua
+++
@@ -1,6 +0,0 @@
-local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BSS-BananaCat.lua"))()
-   else
-if game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BF-BananaCat.lua"))()
-end
